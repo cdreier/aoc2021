e08041129df21061a366fe124fcdc5e3a2811954
Christian Dreier <cdreier@inovex.de>
2021-12-01 09:27:47 +0100

    day one

--- dec01/main.exs
+++ dec01/main.exs
@@ -2,9 +2,28 @@ Code.require_file("lib/files.exs")
 
 defmodule Dec01 do
   import Shortcuts
+  import Enum
 
   def run() do
-    byLine("dec01/input.txt", :int) |> IO.inspect()
+    byLine("dec01/input.txt", :int)
+    |> part2()
+  end
+
+  def part2(input) do
+    input
+    |> chunk_every(3, 1)
+    |> map(&sum(&1))
+    |> part1()
+  end
+
+  def part1(input) do
+    input
+    |> chunk_every(2, 1)
+    |> Stream.filter(&(length(&1) == 2))
+    |> Stream.filter(&(at(&1, 0) < at(&1, 1)))
+    |> to_list()
+    |> count()
+    |> IO.inspect()
   end
 end
 
