c11351178047bdcb5d871c853c0e6ca292149deb
Christian Dreier <cdreier@inovex.de>
2021-12-09 17:32:27 +0100

    i should switch to go

--- dec04/main.exs
+++ dec04/main.exs
@@ -1,11 +1,19 @@
 Code.require_file("lib/files.exs")
 
 defmodule Dec04 do
-  import Shortcuts
+  import Enum
 
   def run() do
-    byLine("dec04/input.txt", :int) |> IO.inspect()
+    {nums, boards} =
+      File.read!("dec04/input.txt") |> String.split("\n", trim: false) |> to_list() |> split(1)
+
+    numbers = at(nums, 0) |> String.split(",", trim: true) |> to_list()
+
+    IO.inspect([numbers, boards])
   end
+end
+
+defmodule Board do
 end
 
 Dec04.run()
--- dec05/main.exs
+++ dec05/main.exs
@@ -4,8 +4,29 @@ defmodule Dec05 do
   import Shortcuts
 
   def run() do
-    byLine("dec05/input.txt", :int) |> IO.inspect()
+    byLine("dec05/input.txt")
+    |> Stream.map(fn e ->
+      String.split(e, " -> ")
+      |> Stream.map(fn str ->
+        String.split(str, ",") |> Stream.map(&String.to_integer/1) |> Enum.to_list()
+      end)
+      |> Enum.to_list()
+    end)
+    |> Stream.filter(&filterCond/1)
+    |> Enum.to_list()
+    |> IO.inspect()
   end
+
+  def drawVetical(map, [[same, y1], [same, y2]]) do
+    [y1..y2]
+    |> Stream.each(fn y ->
+      nil
+      # Map.update(map, same, [], &updateList(&1, y))
+    end)
+  end
+
+  def filterCond([[x1, y1], [x2, y2]]), do: x1 == x2 || y1 == y2
+  def filterCond([_, _]), do: false
 end
 
 Dec05.run()
